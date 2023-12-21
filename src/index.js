534665298c50d65fd81cb80715ea182344c29305
mjvinti <mjvinti@gmail.com>
2023-12-21 09:45:42 -0500

    added user store and scope to config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { AuthProvider } from 'react-oidc-context';
+import { WebStorageStateStore } from 'oidc-client-ts';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -10,7 +11,9 @@ import './index.css';
 const oidcConfig = {
   authority: process.env.REACT_APP_AUTHORITY,
   client_id: process.env.REACT_APP_CLIENT_ID,
-  redirect_uri: process.env.REACT_APP_REDIRECT_URI
+  redirect_uri: process.env.REACT_APP_REDIRECT_URI,
+  userStore: new WebStorageStateStore({ store: window.localStorage }),
+  scope: process.env.REACT_APP_SCOPE
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
